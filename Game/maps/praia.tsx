49117878d2e1a2d4675006386788b4c76d067684
Lucasark <lucas.arkantos@hotmail.com>
2018-09-10 13:51:25 -0300

    Titles

---
+++ Game/maps/praia.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="praia" tilewidth="32" tileheight="32" tilecount="117" columns="13">
+ <image source="tiles/wiptiles.png" width="416" height="288"/>
+</tileset>
