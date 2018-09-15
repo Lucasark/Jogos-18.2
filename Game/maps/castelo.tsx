35d7896ee3401b3260739f8c173f28231fbd1778
Isaac de Souza Moraes da Silva <githubdonego@gmail.com>
2018-09-15 16:21:50 -0300

    mapa da praia(sem colisões ainda)
    fala meu chegado, eu editei o primeiro mapa aí da praia e quero saber o que você acha, claro que a gente pode melhorar muito, eu n sou lá o cara bom do design, eu vou fazer só os mapas sem as colisões por enquanto, porquê ainda gostaria de conversar contigo na hora de fazer elas porquê dependendo de onde eu ponho, pode atrapalhar ou afetar a mecânica geral do game em relação a spawns e etc, daí vou enviar só os mapas, tô enviando o primeiro com alguns tilesets que eu baixei e usei pra editar

---
+++ Game/maps/castelo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="castelo" tilewidth="32" tileheight="32" tilecount="20" columns="4">
+ <image source="tiles/castlefloors_outside.png" width="128" height="160"/>
+</tileset>
