2ecf44883305c34715ecd9c215079bd7a7bd6f54
Lucasark <lucas.arkantos@hotmail.com>
2018-09-17 20:29:24 -0300

    Organização e Jogo Estruturado

---
+++ Game/Area de Criação/map/florst.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="aaaa" tilewidth="32" tileheight="32" tilecount="35" columns="7">
+ <image source="../../../../../Downloads/forest_tiles.png" width="240" height="160"/>
+</tileset>
