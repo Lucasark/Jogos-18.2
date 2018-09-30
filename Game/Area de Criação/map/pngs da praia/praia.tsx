c3dee1d87e4273411aa3d4adca7a9359bd5c96d0
Isaac de Souza Moraes da Silva <githubdonego@gmail.com>
2018-09-30 17:24:04 -0300

    c o é meu parcero, seguinte eu renomeei o nome da pasta "png provisorio da praia" pra "png da praia" pq já ta definido mesmo, só montar os sprites, adicionei também o sprite do personagem na pasta de personagens, alguns monstros na pasta de monstros e criei uma pasta pros recursos e maquinas

---
+++ Game/Area de Criação/map/pngs da praia/praia.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="praia" tilewidth="32" tileheight="32" tilecount="5208" columns="8">
+ <image source="../../Documents/GitHub/Jogos-18.2/Game/Area de Criação/map/tiles/swamp_and_forest_pokemon_based.png" width="256" height="20832"/>
+</tileset>
---
+++ Game/Area de Criação/map/pngs da praia/praia2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="praia2" tilewidth="32" tileheight="32" tilecount="667" columns="23">
+ <image source="../../Documents/GitHub/Jogos-18.2/Game/Area de Criação/map/tiles/tem_flor_praia_deserto_e_ponte.png" width="736" height="928"/>
+</tileset>
