cf7c4c7cca6b6c882ae68fae89bff4bc1e43354a
Isaac de Souza Moraes da Silva <githubdonego@gmail.com>
2018-10-17 16:37:28 -0300

    Os arquivos Game.py e Settings.py estão na pasta "pngs da praia"
    modificações:
    
    função andar() e colisão():
     mudança de ifs para elifs, dado fato de que não se pode andar na diagonal nesse jogo em específico, já que não temos sprites diagonais para o personagem
    
     adicionei uma variavel "tile" que contem o valor 32, graças a sua metodologia de mapeamento, essa variavel foi muito útil na hora de posicionar os elementos no mapa
    
     adicionei também um fundo preto temporario para lidar com o problema das imagens se repetindo infinitamente
    
     corrigi o problema dos objetos colidiveis andando mais rapido que o bg, agora todos andam juntos
    
     como o posicionamento de sprites já está tranquilo agora, já ta tudo certo para apresentar pelo menos o mapa ao professor nessa sexta-feira, entretanto, receio não conseguir corrigir perfeitamente os problemas de colisão, ta foda mesmo.
    
     estou te enviando mais alguns pngs que foram inseridos no mapa(devidamente inseridos no settings) e outros pngs de construção, que apesar de já ter te mostrado, não tinha commitado ainda.
    
     reorganizei um pouco o código e adicionei os comentários(relaxa, não pus >>> ESPAÇO <<<, ta ok?)
    
     tenha uma boa tarde

---
+++ Game/Area de Criação/map/pngs da praia/game.py
@@ -0,0 +1,173 @@
+from PPlay.window import *
+from PPlay.collision import *
+from PPlay.gameimage import *
+import settings as s
+
+#janela
+janela = Window(s.WIDTH, s.HEIGHT)  # 720,480 - 1280,720
+janela.set_background_color((255,255,255))
+janela.set_title(s.TITLE)
+#janela.set_fullscreen()
+background_da_janela = GameImage(s.background_do_background)
+
+#mouse
+mouse = janela.get_mouse()
+
+#fundo
+fundo = GameImage(s.bg)
+
+#teclado
+k = Window.get_keyboard()
+
+#velocidade
+velocidade = 250*janela.delta_time()
+
+#colisao
+colisao_pplay = Collision()
+
+
+
+#TILE - o uso do numero 32, graças a sua motherfucking lógica de uso de tiles ta sendo bem conveniente, então pro professor não ficar "ui ta usando número absoluto" eu to declarando uma variavel
+tile = 32
+
+#SET_POSITION: PLAYER
+
+player = GameImage(s.player)
+player.set_position(s.WIDTH/2, s.HEIGHT/2)
+
+#SET POSITIONS: BACKGROUD
+
+#Monster Spawn
+monster_spawn = GameImage(s.cenario[0])
+monster_spawn.set_position(fundo.width-monster_spawn.width-tile,fundo.height/2)
+
+#pedra de cenário
+pedra_de_5_lados = GameImage(s.pedra)
+pedra_de_5_lados.set_position(fundo.width/2+tile*3,fundo.height/2+tile*3)
+
+#Agua
+agua = GameImage(s.borderEdge[2])
+agua.set_position(0, fundo.height-agua.height)
+
+#Blogueio
+bloqueio1 = GameImage(s.borderEdge[1]) #Superior
+bloqueio2 = GameImage(s.borderEdge[0]) #Lateral-Esq
+bloqueio3 = GameImage(s.borderEdge[0]) #Lateral-Dir
+bloqueio1.set_position(tile, 0)
+bloqueio3.set_position(fundo.width-tile, 0)
+bloqueio2.set_position(0, 0)
+
+#Grama
+grama = GameImage(s.borderEdge[3])
+grama.set_position(0, 0)
+
+#Arvores
+arvore_1 = GameImage(s.cenario[1])
+arvore_2 = GameImage(s.cenario[2])
+arvore_3 = GameImage(s.cenario[3])
+arvore_4 = GameImage(s.cenario[2])
+
+arvore_1.set_position(tile,bloqueio1.height)
+arvore_2.set_position(tile,arvore_1.height)
+arvore_3.set_position(fundo.width-tile*4,bloqueio1.height)
+arvore_4.set_position(arvore_3.x,arvore_3.height)
+
+#Appendes do BG:
+nao_colidiveis = [fundo, grama,monster_spawn]
+colidiveis = [bloqueio1, bloqueio2, bloqueio3,arvore_1,arvore_2,agua,arvore_3,arvore_4,pedra_de_5_lados]
+bg_total = nao_colidiveis + colidiveis
+
+#colisao:
+
+def colisao(atr,player):
+    for i in range(0,len(atr)):
+        if colisao_pplay.collided_perfect(atr[i],player):
+            return True
+    return False
+
+# def colisao(atr,player):
+#     for i in range(0,len(atr)):
+#         if player.x+1 == atr[i].x:
+#             return True
+#         elif player.x-1 == atr[i].x:
+#             return True
+#         elif player.y+1 == atr[i].y:
+#             return True
+#         elif player.y-1 == atr[i].y:
+#             return True
+#     return False
+
+#Appendes do Cenario:
+
+
+
+
+def draw_sprite(atr):
+    for i in range(len(atr)):
+        atr[i].draw()
+
+def movimento_objet(atr, mov, orien):
+    velocidade = 250 * janela.delta_time()
+    if mov == 1 and orien == 'x':
+        for i in range(len(atr)):
+            atr[i].x += velocidade
+
+    elif mov == -1 and orien == 'x':
+        for i in range(len(atr)):
+            atr[i].x -= velocidade
+
+    elif mov == 1 and orien == 'y':
+        for i in range(len(atr)):
+            atr[i].y -= velocidade
+
+    elif mov == -1 and orien == 'y':
+        for i in range(len(atr)):
+            atr[i].y += velocidade
+
+def andar():
+    if k.key_pressed("A") or k.key_pressed("LEFT"): #<-
+        '''
+        if (fundo.x + player.x >= 0):
+            player.x -= 1
+        else:
+        '''
+        if not colisao(colidiveis,player):
+            movimento_objet(bg_total, 1, 'x')
+        else:
+            movimento_objet(bg_total, -1,'x')
+
+
+    elif k.key_pressed("D") or k.key_pressed("RIGHT"): #->
+        '''
+        if (fundo.x + player.x <= 0 ):
+            player.x += 1
+        else:
+        '''
+        if not colisao(colidiveis,player):
+            movimento_objet(bg_total, -1, 'x')
+        else:
+            movimento_objet(bg_total, 1,'x')
+
+    elif k.key_pressed("S") or k.key_pressed("DOWN"):
+        if not colisao(colidiveis, player):
+            movimento_objet(bg_total, 1, 'y')
+        else:
+            movimento_objet(bg_total,-1,'y')
+    elif k.key_pressed("W") or k.key_pressed("UP"):
+        if not colisao(colidiveis, player):
+            movimento_objet(bg_total, -1, 'y')
+        else:
+            movimento_objet(bg_total,1,'y')
+
+#print(player.x, ":X - Y:", player.y,"     ",s.WIDTH,":S.W  -  F.W",fundo.width,"         ",fundo.x,":f.x   -  f.y",fundo.y)
+
+def update():
+    janela.set_title(str(janela.delta_time()))
+    andar()
+    player.draw()
+    janela.update()
+
+while True:
+    background_da_janela.draw()
+    draw_sprite(bg_total)
+    update()
---
+++ Game/Area de Criação/map/pngs da praia/settings.py
@@ -0,0 +1,36 @@
+import pygame
+
+#Titulo
+TITLE = 'Teste de Game'
+
+
+
+#Dificuldade
+DIFICULDADE_GAME = 1
+
+#Tela
+# Suporte no 16:9 -> 720,480 - 1280,720
+WIDTH = 720
+HEIGHT = 480
+
+#PNG
+
+#MENU
+bg = 'bg_praia.png'
+arvore = ['arvore1.png', 'arvore2.png', 'arvore3.png']
+pedra = 'pedra_de_5_lados.png'
+borderEdge = ['bloqueio_lateral.png', 'bloqueio_superior.png', 'agua_praia.png', 'grama_praia.png']
+cenario = ['monster_spawn.png', 'arvore1.png', 'arvore2.png', 'arvore3.png']
+player = 'tile001.png'
+background_do_background = 'fundo_forcado.png'
+
+#Buttoes
+#menus = ['iniciar.png', 'placar.png', 'dificuldade.png', 'sair.png']
+#menusP = ['iniciarP.png', 'placarP.png', 'dificuldadeP.png', 'sairP.png']
+#DIF = ['D1.png','D2.png','D3.png']
+#DIFP = ['D1P.png','D2P.png','D3P.png']
+#DIFS = ['D1S.png','D2S.png','D3S.png']
+#VOLTAR = 'voltar.png'
+#VOLTARP = 'voltarP.png'
+
+#PLAYER SETTINGS:
