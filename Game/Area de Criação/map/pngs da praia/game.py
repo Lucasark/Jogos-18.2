e4e7cd61202c293cb40fbf81dea3afedfcd68a28
lucasark <lucas.arkantos@hotmail.com>
2018-11-12 12:50:24 -0200

    Button e Collisons

--- Game/Area de Criação/map/pngs da praia/game.py
+++ Game/Area de Criação/map/pngs da praia/game.py
@@ -2,10 +2,12 @@ from PPlay.window import *
 from PPlay.collision import *
 from PPlay.gameimage import *
 import settings as s
+#import options as o
+#import classes as c
 
 #janela
 janela = Window(s.WIDTH, s.HEIGHT)  # 720,480 - 1280,720
-janela.set_background_color((255,255,255))
+janela.set_background_color((255, 255, 255))
 janela.set_title(s.TITLE)
 #janela.set_fullscreen()
 background_da_janela = GameImage(s.background_do_background)
@@ -73,14 +75,77 @@ arvore_2.set_position(tile,arvore_1.height)
 arvore_3.set_position(fundo.width-tile*4,bloqueio1.height)
 arvore_4.set_position(arvore_3.x,arvore_3.height)
 
+#Habilidades:
+"""
+
+[-1]    ->  Bloqueados: Imagem preta [SN1], e não está disponivel para compra
+[1]     ->  Comprados: Imagem Cinza [CI] , já foi adquirido
+[0]     ->  Liberados: Imagem Colorida [CO], liberado para compra
+
+"""
+habilidade_atual = [GameImage(s.COH[0]), GameImage(s.SNH[1]), GameImage(s.SNH[2]), GameImage(s.SNH[4]), GameImage(s.SNH[5]), GameImage(s.SNH[6]),
+                    GameImage(s.SNH[7]), GameImage(s.SNH[8]), GameImage(s.SNH[9]), GameImage(s.SNH[10]), GameImage(s.SNH[11]), GameImage(s.SNH[12]),
+                    GameImage(s.SNH[13]), GameImage(s.SNH[14]), GameImage(s.SNH[15]), GameImage(s.SNH[16]), GameImage(s.SNH[17]), GameImage(s.SNH[18])]
+
 #Appendes do BG:
-nao_colidiveis = [fundo, grama,monster_spawn]
+nao_colidiveis = [fundo, grama, monster_spawn]
 colidiveis_esq = [bloqueio2]
 colidiveis_dir = [bloqueio3]
 colidiveis_cima = [bloqueio1]
 colidiveis_baixo = [agua]
 colidiveis = [arvore_1, arvore_2, arvore_3, arvore_4, pedra_de_5_lados]
 bg_total = nao_colidiveis + colidiveis + colidiveis_cima + colidiveis_baixo + colidiveis_dir + colidiveis_esq
+
+#centralizar objeto em relacao a janela
+def centralizar_w(obj, quadro):
+    return quadro.width/2 - (obj.width/2)
+
+def centralizar_h(obj, quadro):
+    return quadro.height/2 - (obj.height/2)
+
+#Habilidades:
+habilidade = GameImage(s.bg_habilidades)
+habilidade.set_position(centralizar_w(habilidade, janela), centralizar_h(habilidade, janela))
+
+#Ferramentas:
+ferramenta = GameImage(s.bg_ferramenta)
+ferramenta.set_position(centralizar_w(ferramenta, janela), centralizar_h(ferramenta, janela))
+
+#posicionar objeto Xp abaixo em relativo a outro obj
+def pos_relativo_h(rel, y):
+    return rel.y + rel.height + y
+
+def pos_relativo_w(rel, x):
+    return rel.x + rel.width + x
+
+def pos_relativo_h_neg(rel, obj, y):
+    return rel.y - obj.height - y
+
+def pos_relativo_w_neg(rel, obj, x):
+    return rel.x - obj.width - x
+
+#HABILIDAEDES POSIÇÃO:
+habilidade_atual[0].set_position(centralizar_w(habilidade_atual[0],janela), centralizar_h(habilidade_atual[0], janela))
+habilidade_atual[1].set_position(habilidade_atual[0].x, pos_relativo_h_neg(habilidade_atual[0], habilidade_atual[1], 32))
+habilidade_atual[2].set_position(pos_relativo_w(habilidade_atual[0], 16), pos_relativo_h(habilidade_atual[0], 16))
+habilidade_atual[3].set_position(pos_relativo_w_neg(habilidade_atual[0],habilidade_atual[3], 16), pos_relativo_h(habilidade_atual[0], 16))
+habilidade_atual[4].set_position(habilidade_atual[3].x, pos_relativo_h_neg(habilidade_atual[1], habilidade_atual[4], 32))
+habilidade_atual[5].set_position(habilidade_atual[2].x, pos_relativo_h_neg(habilidade_atual[1], habilidade_atual[4], 32))
+habilidade_atual[6].set_position(pos_relativo_w(habilidade_atual[2], 32), habilidade_atual[2].y)
+habilidade_atual[7].set_position(habilidade_atual[2].x, pos_relativo_h(habilidade_atual[2], 32))
+#habilidade_atual[8].set_position()
+#habilidade_atual[9].set_position()
+#habilidade_atual[10].set_position()
+#habilidade_atual[11].set_position()
+#habilidade_atual[12].set_position()
+habilidade_atual[13].set_position(pos_relativo_w(habilidade_atual[6], 32), habilidade_atual[6].y)
+habilidade_atual[14].set_position(habilidade_atual[6].x + (habilidade_atual[6].width)/2, habilidade_atual[7].y + (habilidade_atual[7].height)/2)
+habilidade_atual[15].set_position(habilidade_atual[7].x, pos_relativo_h(habilidade_atual[7], 32))
+#habilidade_atual[16].set_position()
+#habilidade_atual[17].set_position()
+#habilidade_atual[18].set_position()
+
+#FERRAMENTAS POSIÇÃO
 
 #Button
 def butao_click(mouse, butao):
@@ -103,6 +168,22 @@ def click_call():
             s.CLICK_TIME -= 1
         else:
             s.CLICK_TIME = 20
+
+def trocar_sprite_hab(old, pos ,op):
+    if op == 1:
+        aux = GameImage(s.COH[pos])
+        aux.set_position(old.x, old.y)
+        return aux
+    elif op == 0:
+        aux = GameImage(s.CIH[pos])
+        aux.set_position(old.x, old.y)
+        return aux
+    elif op == -1:
+        aux = GameImage(s.SNH[pos])
+        aux.set_position(old.x, old.y)
+        return aux
+    else:
+        return 0
 
 #Collisions
 def colisao_direita(atr,player):
@@ -176,7 +257,6 @@ def colisao_baixo(atr,player):
 
 #Appendes do Cenario:
 
-
 def draw_sprite(atr):
     for i in range(len(atr)):
         atr[i].draw()
@@ -229,23 +309,99 @@ def andar():
         else:
             movimento_objet(bg_total,1 ,'y')
 
+def liberar_habilidade(hab):
+    for i in range(len(hab)):
+        if s.HABILIDADE_STATUS[hab[i]] == -1:
+            print("Habilidade",hab[i],"Liberada")
+            habilidade_atual[hab[i]] = trocar_sprite_hab(habilidade_atual[hab[i]], hab[i], 1)
+            s.HABILIDADE_STATUS[hab[i]] = 0
+
+def habilidade_action(act):
+    if act == 0:
+        print("HABILIDADE 0 EFTUADO")
+    elif act == 1:
+        print("HABILIDADE 1 EFTUADO")
+    elif act == 2:
+        print("HABILIDADE 2 EFTUADO")
+    elif act == 3:
+        print("HABILIDADE 3 EFTUADO")
+    elif act == 4:
+        print("HABILIDADE 4 EFTUADO")
+    elif act == 5:
+        print("HABILIDADE 5 EFTUADO")
+    elif act == 6:
+        print("HABILIDADE 6 EFTUADO")
+    elif act == 7:
+        print("HABILIDADE 7 EFTUADO")
+    elif act == 8:
+        print("HABILIDADE 8 EFTUADO")
+    elif act == 9:
+        print("HABILIDADE 9 EFTUADO")
+    elif act == 10:
+        print("HABILIDADE 10 EFTUADO")
+    elif act == 11:
+        print("HABILIDADE 11 EFTUADO")
+    elif act == 12:
+        print("HABILIDADE 12 EFTUADO")
+    elif act == 13:
+        print("HABILIDADE 13 EFTUADO")
+    elif act == 14:
+        print("HABILIDADE 14 EFTUADO")
+    elif act == 15:
+        print("HABILIDADE 15 EFTUADO")
+    elif act == 16:
+        print("HABILIDADE 16 EFTUADO")
+    elif act == 17:
+        print("HABILIDADE 17 EFTUADO")
+    elif act == 18:
+        print("HABILIDADE 18 EFTUADO")
+    else:
+        print("ZUOU")
+
 def opcoes():
-    if k.key_pressed("F") and click_pause(s.CLICK_TIME_WAIT):
-        print("FERRAMENTAS")
-    elif k.key_pressed("E") and click_pause(s.CLICK_TIME_WAIT):
+    m = mouse.get_position()
+    if k.key_pressed("F") and click_pause(s.CLICK_TIME_WAIT) and not s.ferramenta_press:
         print("HABILIDADES")
+        if (not s.habilidade_press):
+            s.habilidade_press = True
+        else:
+            s.habilidade_press = False
+
+    elif k.key_pressed("E") and click_pause(s.CLICK_TIME_WAIT) and not s.habilidade_press:
+        print("FERRAMENTAS")
+        if (not s.ferramenta_press):
+            s.ferramenta_press = True
+        else:
+            s.ferramenta_press = False
+
     elif k.key_pressed("ESC") and click_pause(s.CLICK_TIME_WAIT):
         print("sair")
         janela.close()
 
+    if s.habilidade_press:
+        habilidade.draw()
+        draw_sprite(habilidade_atual)
+        for i in range(len(habilidade_atual)):
+            if butao_click(m, habilidade_atual[i]):  # 1
+                #printar balão
+                if mouse.is_button_pressed(1) and click_pause(s.CLICK_TIME_WAIT) and s.HABILIDADE_STATUS[i] == 0:
+                    print("Habilidade", i,"Desloqueada")
+                    habilidade_action(i)
+                    habilidade_atual[i] = trocar_sprite_hab(habilidade_atual[i], i, 0)
+                    s.HABILIDADE_STATUS[i] = -1
+                    liberar_habilidade(s.HABILIDADE_LIBERAR[i])
+                    print(s.HABILIDADE_STATUS)
+
+    if s.ferramenta_press:
+        ferramenta.draw()
 
 #print(player.x, ":X - Y:", player.y,"     ",s.WIDTH,":S.W  -  F.W",fundo.width,"         ",fundo.x,":f.x   -  f.y",fundo.y)
 
 def update():
     janela.set_title(str(janela.delta_time()))
     andar()
-    opcoes()
     player.draw()
+    opcoes()
     janela.update()
 
 while True:
--- Game/Area de Criação/map/pngs da praia/settings.py
+++ Game/Area de Criação/map/pngs da praia/settings.py
@@ -6,21 +6,61 @@ TITLE = 'Teste de Game'
 #Dificuldade
 DIFICULDADE_GAME = 1
 
+
 #Tela
 # Suporte no 16:9 -> 720,480 - 1280,720
-WIDTH = 720
-HEIGHT = 480
+WIDTH = 1280
+HEIGHT = 720
 
 #PNG
 
 #MENU
+
+#CENARIO 1:
 bg = 'bg_praia.png'
 arvore = ['arvore1.png', 'arvore2.png', 'arvore3.png']
 pedra = 'pedra_de_5_lados.png'
 borderEdge = ['bloqueio_lateral.png', 'bloqueio_superior.png', 'agua_praia.png', 'grama_praia.png']
 cenario = ['monster_spawn.png', 'arvore1.png', 'arvore2.png', 'arvore3.png']
-player = 'tile001.png'
 background_do_background = 'fundo_forcado.png'
+
+#CENARIO 2:
+
+#CENARIO 3:
+
+#HABILIDAEDS:
+bg_habilidades = "bg_habilidades.png"
+habilidade_press = False
+#SNH = 'SNH1.png'
+
+#Bloqueado
+SNH = ['SNH1.png', 'SNH1.png', 'SNH1.png', 'SNH1.png', 'SNH1.png', 'SNH1.png', 'SNH1.png',
+       'SNH1.png', 'SNH1.png', 'SNH1.png', 'SNH1.png', 'SNH1.png', 'SNH1.png', 'SNH1.png',
+       'SNH1.png', 'SNH1.png', 'SNH1.png', 'SNH1.png', 'SNH1.png', 'SNH1.png', 'SNH1.png']
+#Comprados
+CIH = ['CIH1.png', 'CIH1.png', 'CIH1.png', 'CIH1.png', 'CIH1.png', 'CIH1.png', 'CIH1.png',
+       'CIH1.png', 'CIH1.png', 'CIH1.png', 'CIH1.png', 'CIH1.png', 'CIH1.png', 'CIH1.png',
+       'CIH1.png', 'CIH1.png', 'CIH1.png', 'CIH1.png', 'CIH1.png', 'CIH1.png', 'CIH1.png']
+
+#Livres
+COH = ['COH1.png', 'COH1.png', 'COH1.png', 'COH1.png', 'COH1.png', 'COH1.png', 'COH1.png',
+       'COH1.png', 'COH1.png', 'COH1.png', 'COH1.png', 'COH1.png', 'COH1.png', 'COH1.png',
+       'COH1.png', 'COH1.png', 'COH1.png', 'COH1.png', 'COH1.png', 'COH1.png', 'COH1.png']
+
+HABILIDADE_STATUS = [0, -1, -1, -1, -1, -1,
+                     -1, -1, -1, -1, -1, -1,
+                     -1, -1, -1, -1, -1, -1]
+
+HABILIDADE_LIBERAR = [[1,2,3],[4,5],[6,7],[8,9],[10,11],[11,12],[13,14],[14,15],[16,17],[17,18],
+                      [],[],[],[],[],[],[],[],[]]
+
+HABILIDADE_TURN = [False, False, False, False]
+
+
+#FERRAMENTAS:
+bg_ferramenta = "bg_habilidades.png"
+ferramenta_press = False
+
 
 #Buttoes
 #menus = ['iniciar.png', 'placar.png', 'dificuldade.png', 'sair.png']
@@ -32,6 +72,7 @@ background_do_background = 'fundo_forcado.png'
 #VOLTARP = 'voltarP.png'
 
 #PLAYER SETTINGS:
+player = 'tile001.png'
 andar_cima = True
 andar_baixo = True
 andar_esquerda = True
@@ -43,5 +84,8 @@ CLICK_TIME_WAIT = 30
 CLICK_ANDAR_WAIT = 20
 CLICK_SIGNAL = True #True -> Pode clickar -- False-> Nao pode clickar
 
+#PLAYER PARAMETROS:
+VIDA = 0
+EXPERIENCIA = 0
+TIME_TO_EXPERIENCIA = 0
 
-#PLAYER SETTINGS:
